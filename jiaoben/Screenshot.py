51eae75437692ab67bb649c94158a5a58bcf387e
dapeng <dapeng@qufaya.com>
2018-11-17 21:06:26 +0800

    jiaoben

---
+++ jiaoben/Screenshot.py
@@ -0,0 +1,21 @@
+import os
+import itertools
+
+
+class Screenshot():
+
+    def Screenshot(self):
+        i = 1
+        mylist = ("".join(x) for x in
+                  itertools.product("123abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", repeat=16))
+        while i < 500:
+            a = (next(mylist))
+            i = i + 1
+            # os.system("adb shell screencap -p /sdcard/%s.png" % (a))
+            os.system("adb shell screencap -p /sdcard/DCIM/Screenshots/%s.png" % (a))
+            # print(a)
+
+
+if __name__ == '__main__':
+    c = Screenshot()
+    c.Screenshot()
